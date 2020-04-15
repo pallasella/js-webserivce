e65e723fc3a9e281165202887fb49a74319fb3bc
pallasella <7777777cute@naver.com>
2020-04-16 01:48:17 +0900

    prtfplio

---
+++ prtfplio/prtfplio.js
@@ -0,0 +1,26 @@
+/*HEADER*/
+window.onload = function() {this.scrollFunction()};
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    var header = document.getElementById('header');
+
+    if(document.documentElement.scrollTop>70){
+        if(!header.classList.contains('navbar-fixed')){
+            header.classList.add('navbar-fixed');
+            document.getElementsByTagName('body')[0].style.marginTop='70px';
+            header.style.display = 'none';
+            setTimeout(function(){
+                header.style.display = 'block';
+            },40);
+        }
+    } else {
+        if(header.classList.contains('navbar-fixed')){
+            header.classList.remove('navbar-fixed');
+            document.getElementsByName('body')[0].style.marginTop = '0';
+        }
+    }
+}
+function menuToggle(){
+    document.getElementById('menu').classList.toggle('show');
+}
+document.getElementById('toggleBtn').addEventListener('click,menuToggle');
